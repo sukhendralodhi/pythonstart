0398792f0eef568fda6854f9fbf8bf4b5adaa68f
sukhendralodhi <developer.sukhendra@gmail.com>
2025-04-02 02:06:22 +0530

    Variable practice done

--- hello.py
+++ hello.py
@@ -1,3 +1,66 @@
-import sys;
+# import sys;
 
-print(sys.version);
+# print(sys.version);
+
+# num1 = 10;
+# num2 = 40;
+
+# if(num1>num2):
+#     print(f'Yes {num2} is greater');
+# else:
+#     print(f'No {num1} is not grater');
+
+
+# x = 10;
+# print(type(x));
+
+# y = 'Sanju';
+# print(type(y));
+
+
+# x,y,z = 'orange', 'mango', 'banana';
+# print(x);
+# print(y);
+# print(z);
+
+
+fruits = ['orange', 'mango', 'banana'];
+x,y,z = fruits;
+
+# print(x);
+# print(y);
+# print(z);
+
+a = "Python is awesome";
+# print(a);
+
+m = 'Python';
+n = 'Is';
+o = 'Awesome';
+
+# print(m);
+# print(n);
+# print(o);
+
+# print(m +  n + o);
+
+# name = 'sanju';
+
+# def myName():
+#     global name;
+#     name = 'Gayu';
+#     print(f'My name is {name}');
+
+# myName();
+# print(name);
+
+
+# nam1 = nam2 = nam3 = 'Hello Sanju';
+
+# print(nam3);
+
+# print("Hello", "World");
+
+z = range(5,10);
+
+# print(type(z));
