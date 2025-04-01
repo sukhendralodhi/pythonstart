1e36830d92796de2b309d4cd8b5680fc58c1204b
sukhendralodhi <developer.sukhendra@gmail.com>
2025-04-02 01:15:44 +0530

    First python repo

---
+++ main.py
@@ -0,0 +1,19 @@
+def addNumer(num1, num2):
+    return num1 + num2
+
+num1 = 4;
+num2 = 5;
+
+output = addNumer(num1, num2);
+
+# print(output);
+
+def printName(fname: str, lname: str):
+    return fname.lower() + ' ' + lname  # Convert boolean to string before concatenation
+
+fname = 'SANJU'
+lname = 'Rajpoot'
+
+output = printName(fname, lname)
+
+print(output)
